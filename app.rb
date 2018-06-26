ef04d8e27efe3cbf4128f195de7335980979ec53
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-26 16:20:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,14 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Jeremiah"
   end
+
+  get '/hometown' do
+    "My hometown is Wheaton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Sober'"
+  end
+
+
 end
